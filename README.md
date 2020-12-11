c511e476515e0e1f85bf07a00c41f5eacbe696af
Joseph Romano <jdromano2@gmail.com>
2020-12-11 12:13:26 -0500

    Rename repository and create executable

---
+++ README.md
@@ -0,0 +1,10 @@
+# GraphDbETL
+
+An ETL (Extract-Transform-Load) toolkit for graph databases.
+
+Specifically, GraphDbETL is designed to make it easier to integrate data from
+public third-party databases (especially relational and flat databases) into
+a single graph database.
+
+This project is motivated by [ComptoxAI](https://comptox.ai/), and the 
+challenges associated with building its central graph database.
---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fp:
+    long_description = fp.read()
+
+setuptools.setup(
+    name="PyGraphETL-jdromano2",
+    version="0.0.1a",
+    author="Joseph D. Romano",
+    author_email="jdromano2@gmail.com",
+    description="A command line toolkit for performing ETL on graph databases",
+    long_description=long_description,
+    url="https://github.com/jdromano2/graphdbetl",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: Unix",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Database",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence"
+    ],
+    python_requires='>=3.8',
+)
